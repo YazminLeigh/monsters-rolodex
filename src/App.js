9671ab87731b9d1d07df61ba263ba85ea0cad4cd
Yazmin Needham <80151019+YazminLeigh@users.noreply.github.com>
2021-09-28 11:21:33 +0100

    Card component added

--- src/App.js
+++ src/App.js
@@ -22,11 +22,7 @@ componentDidMount() {
   render() {
     return (
       <div className="App">
-        < CardList className='App'> 
-          {this.state.monsters.map(monster => (
-            <h1 key={monster.id}> {monster.name} </h1>
-          ))}
-          </CardList>
+        < CardList monsters={this.state.monsters} />
       </div>
   );
 }
